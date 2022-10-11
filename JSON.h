a504fd49770511aa9e85b3156fe945dc68f0d640
elip140 <epcotrim@gmail.com>
2022-10-11 17:32:24 -0300

    Struct

---
+++ JSON.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct Funcionario{
+	int Codigo;
+	char Nome[100];
+	char Ocupacao[100];
+	
+};
+
+struct Log{
+	int Cod_Func;
+	char Horario[100];
+	char Finalidade[100];
+};
+
+void Teste(){
+	printf("TESTANDO FUNÇÃO");
+}
